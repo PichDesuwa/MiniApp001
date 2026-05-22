0d063eb2cc16fce1330815907f5fdd0b463a89a1
mAiyc <lyzatouy@gmail.com>
2026-05-22 14:12:34 +0700

    asdfghjkl

---
+++ Controller.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button button_calculate;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    private TextField textfield_num1;
+
+    @FXML
+    private TextField textfield_num2;
+
+    @FXML
+    void on_calculate(ActionEvent event) {
+
+    }
+
+}
