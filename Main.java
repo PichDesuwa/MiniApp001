7fc249157b1064c2e9fc9c5c47d516b165683a1c
mAiyc <lyzatouy@gmail.com>
2026-05-29 13:54:49 +0700

    ertyu

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("View.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
         stage.show();
     }
 
